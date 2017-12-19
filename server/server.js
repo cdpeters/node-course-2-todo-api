cdbb0059a97a092741e6a05511553da5dfe91413
Chris Peterson <cdpeters1@gmail.com>
2017-12-19 03:18:09 -0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -27,9 +27,12 @@ app.post('/todos', (req, res) => {
 
 // resource reading: GET /todos
 
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
 
 // CRUD operations:
   // - create
