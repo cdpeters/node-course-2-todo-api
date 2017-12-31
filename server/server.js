1f970d6be98490e95fbd072debb24cc6012ec484
Chris Peterson <cdpeters1@gmail.com>
2017-12-30 20:11:09 -0800

    Added async/await to some routes

--- server/server.js
+++ server/server.js
@@ -67,25 +67,27 @@ app.get('/todos/:id', authenticate, (req, res) => {
 });
 
 // deleting specific todos by id
-app.delete('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req, res) => {
+  const id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo) => {
+  try {
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+
     if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
 // updating specific todos by id
@@ -119,21 +121,20 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 });
 
 // PUBLIC ROUTE: SIGN UP: creating new users
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
+app.post('/users', async (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
 
   // User.findByToken - a model method, a method applied to a model
   // user.generateAuthToken - an instance method, a method applied to an instance (thus it requires the document).
-
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
-    // x- creates a custom header in this case = 'auth' that we can access for requests along private routes using req.header('x-auth')
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+      // x- creates a custom header in this case = 'auth' that we can access for requests along private routes using req.header('x-auth')
     res.header('x-auth', token).send(user);
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send(e);
-  });
+  }
 });
 
 // returns the user information: _id and email. This is a private route because of the authenticate middleware.
@@ -141,24 +142,25 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    });
-  }).catch((e) => {
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+// async returns a promise!
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  }, () => {
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
 
