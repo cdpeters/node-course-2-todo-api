9cf8167d8b5591d213d4b7e6b15c6879ba522392
Chris Peterson <cdpeters1@gmail.com>
2017-12-31 21:27:16 -0800

    Added stinky semicolon

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const {authenticate} = require('./middleware/authenticate')
+const {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
